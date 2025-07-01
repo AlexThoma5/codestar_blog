7e8cd105a36ff7fed49357e0956b00c8db1f2d41
Alex <alexthomas1912@gmail.com>
2025-07-02 00:28:39 +0100

    Add env.py file

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7e@az1n8w_qo)m(l$-at&a-u(x(*y%*-jkqqo-l!5h*_8mb8$r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com',]
 
